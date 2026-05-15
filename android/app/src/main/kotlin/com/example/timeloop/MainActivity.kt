40fc384bb47ead467dab207a24dbdfb2de77dddb
Dhruv Haldar <haldardhruv@gmail.com>
2026-05-15 07:38:04 -0700

    feat: add AppImage packaging configuration and assets for Linux distribution

--- android/app/src/main/kotlin/com/example/timeloop/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.timeloop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/dev/dhruv/timeloop/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.dhruv.timeloop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
